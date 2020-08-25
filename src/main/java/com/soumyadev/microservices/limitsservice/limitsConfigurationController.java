6925d654f517e8ecb5e5e640a5ff27ee5a1e2bdd
Soumya Mondal <s.mondal8@gmail.com>
2020-08-25 14:19:51 +0530

    update regarding confg prop

--- src/main/java/com/soumyadev/microservices/limitsservice/limitsConfigurationController.java
+++ src/main/java/com/soumyadev/microservices/limitsservice/limitsConfigurationController.java
@@ -1,5 +1,17 @@
 package com.soumyadev.microservices.limitsservice;
 
-public class limitsConfigurationController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class limitsConfigurationController {
+	@Autowired
+	ConfigurationProps configuration;
+	@GetMapping("/limits")
+	LimitConfiguration retriveLimit() {
+		//return new LimitConfiguration(1000,1);
+		return new LimitConfiguration(configuration.getMaximum()
+				, configuration.getMinimum());
+	}
 }
